513db91a7c1e3ef3913cad136eb4404f0ee808eb
Honour368 <honourolatunji@gmail.com>
2023-05-29 23:43:50 +0800

    Possible Fix to CORS Issues

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ var app = express();
 app.use((req, res, next)=>{
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-    // res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Headers", "*");
     next();
 });
 // app.use(express.bodyParser());
